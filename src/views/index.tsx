50bdbb4d7330654e69fc60626aa168fd532fc7a2
Daniel Garay <garay.daniel@protonmail.com>
2022-03-12 21:24:53 -0500

    deployment error

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,6 +1,6 @@
 export { HomeView } from "./home";
 export { CreateView } from "./create";
-export { AccountView } from "./account";
+export { AccountView } from "./account/index";
 export { MarketView } from "./market";
 export { ContractView } from "./contract";
 export { OwnedView } from "./account/owned";
