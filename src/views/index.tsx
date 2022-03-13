41630fdccf74be2f9663d2d1efa960b27171f0f0
Daniel Garay <garay.daniel@protonmail.com>
2022-03-12 21:49:06 -0500

    pls

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,8 +1,8 @@
-//@ts-ignore
 export { HomeView } from "./home";
 export { CreateView } from "./create";
 export { MarketView } from "./market";
 export { ContractView } from "./contract";
 export { OwnedView } from "./owned";
 export { ApproverView } from "./approvers";
+//@ts-ignore
 export { AccountView } from "./account";
