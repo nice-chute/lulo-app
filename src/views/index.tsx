25dd9c7db8bb0b67366525122417a0e2a7b3bbd1
Daniel Garay <garay.daniel@protonmail.com>
2022-03-12 21:28:29 -0500

    deployment

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,7 +1,7 @@
 export { HomeView } from "./home";
 export { CreateView } from "./create";
-export { AccountView } from "./account/index";
 export { MarketView } from "./market";
 export { ContractView } from "./contract";
 export { OwnedView } from "./account/owned";
 export { ApproverView } from "./account/approvers";
+export { AccountView } from "./account";
